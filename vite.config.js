42386d03710edbd8ae55ba7853ea46edc4696ef0
RyoSugimoto <1042374+RyoSugimoto@users.noreply.github.com>
2024-11-27 16:18:15 +0900

    Updating Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tsconfigPaths(),
+        react(),
     ],
 });
