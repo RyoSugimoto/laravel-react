b1b076b3f454992856358c2acc33bea5824ced89
RyoSugimoto <1042374+RyoSugimoto@users.noreply.github.com>
2024-12-21 15:55:30 +0900

    Set language value when registration

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+use App\Services\Language;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -35,6 +36,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'language' => Language::get(),
         ]);
     }
 }
